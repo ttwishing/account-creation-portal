3736c80b1461364ad61f8b058c1487666f98d7f5
dafuga <danielfugere28@gmail.com>
2024-07-10 13:33:26 -0700

    fix: allowing csrf for apple callback route

--- src/auth.ts
+++ src/auth.ts
@@ -2,10 +2,44 @@ import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/sveltekit/providers/google"
 import Apple from "@auth/sveltekit/providers/apple"
 import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, AUTH_APPLE_ID, AUTH_APPLE_SECRET, AUTH_REDIRECT_URL } from "$env/static/private"
+import { error, json, text, type Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 
 const redirectUrl = AUTH_REDIRECT_URL || "http://localhost:3000"
 
-export const { handle, signIn } = SvelteKitAuth({
+const ALLOWED_PATHS = ['/auth/callback/apple'];
+
+/**
+ * Custom CSRF protection with the ability to bypass for specific routes.
+ */
+const csrf = (): Handle => async ({ event, resolve }) => {
+  const forbidden =
+    event.request.method === 'POST' &&
+    event.request.headers.get('origin') !== event.url.origin &&
+    isFormContentType(event.request) &&
+    !ALLOWED_PATHS.includes(event.url.pathname);
+
+  if (forbidden) {
+    const message = `Cross-site ${event.request.method} form submissions are forbidden`;
+    if (event.request.headers.get('accept') === 'application/json') {
+      return json({ message }, { status: 403 });
+    }
+    return text(message, { status: 403 });
+  }
+
+  return resolve(event);
+};
+
+function isContentType(request: Request, ...types: string[]) {
+  const type = request.headers.get('content-type')?.split(';', 1)[0].trim() ?? '';
+  return types.includes(type);
+}
+
+function isFormContentType(request: Request) {
+  return isContentType(request, 'application/x-www-form-urlencoded', 'multipart/form-data');
+}
+
+const authHandle = SvelteKitAuth({
   trustHost: true,
   providers: [
     Google({
@@ -19,4 +53,9 @@ export const { handle, signIn } = SvelteKitAuth({
       redirectProxyUrl: `${redirectUrl}/auth/callback/apple`,
     })
   ],
-})
+});
+
+// Combine our custom CSRF protection with SvelteKitAuth
+export const handle = sequence(csrf(), authHandle.handle);
+
+export const { signIn, signOut } = authHandle;
--- svelte.config.js
+++ svelte.config.js
@@ -15,10 +15,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		csrf: {
+			checkOrigin: false,
+		}
 	},
 	define: {
 		'process.env': process.env
-	}
+	},
 };
 
 export default config;
